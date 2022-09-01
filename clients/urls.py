7da186e7b2526af9668ec3b415db0af1d3dbe223
Yryskeldi Azamatov <azamatov0202@gmail.com>
2022-09-01 18:26:48 +0600

    hw6

--- clients/urls.py
+++ clients/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clients/', views.clients_list, name='clients'),
-    path('orders/', views.orders_list, name='orders'),
-    path('order/<int:id>/', views.order_detail, name='order-detail'),
-    path('order/update/<int:id>/', views.order_update, name='order-update'),
-    path('order/delete/<int:id>/', views.order_delete, name='order-delete'),
-    path('order/djangoform/', views.order_djangoform, name='order-djangoform'),
+    path('clients/', views.ClientListView.as_view(), name='clients'),
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order-detail'),
+    path('order/update/<int:pk>/', views.OrderUpdateView.as_view(), name='order-update'),
+    path('order/delete/<int:pk>/', views.OrderDeleteView.as_view(), name='order-delete'),
+    path('order/djangoform/', views.CreateOrderView.as_view(), name='order-djangoform'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,57 +1,41 @@
 from django.shortcuts import render, HttpResponse
 from .models import Client, Order
 from .forms import OrderForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import View
 
 # Create your views here.
 
 
-def clients_list(request):
-    context = {}
-    customer_list = Client.objects.all()
-    context["clients_list"] = customer_list
-    html_page = render(request, 'clients/clients.html', context)
-    return html_page
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients/clients.html'
 
 
-def orders_list(request):
-    context = {}
-    order_list = Order.objects.all()
-    context["orders_list"] = order_list
-    html_page = render(request, 'clients/orders.html', context)
-    return html_page
+class OrderListView(ListView):
+    model = Order
+    template_name = 'clients/orders.html'
 
 
-def order_detail(request, id):
-    context = {
-        "order": Order.objects.get(id=id)
-    }
-    return render(request, 'clients/order_details.html', context)
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = "clients/order_details.html"
 
 
-def order_update(request, id):
-    context = {}
-    order_object = Order.objects.get(id=id)
-    if request.method == "POST":
-        form = OrderForm
-    context["form"] = OrderForm(instance=order_object)
-    return render(request, 'clients/order_update.html', context)
+class OrderUpdateView(UpdateView):
+    model = Order
+    fields = ["name", "contacts", "description"]
+    template_name_suffix = '_update'
+    success_url = "/orders/"
 
 
-def order_delete(request, id):
-    context = {}
-    order_object = Order.objects.get(id=id)
-    context['order'] = Order
-    order_object.delete()
-    return render(request, 'clients/order_deletion.html', context)
+class OrderDeleteView(DeleteView):
+    model = Order
+    success_url = "/orders/"
 
 
-def order_djangoform(request):
-    context = {}
-    if request.method == "POST":
-        order_form = OrderForm(request.POST)
-        if order_form.is_valid():
-            order_form.save()
-            return HttpResponse("Форма обработана")
-        return HttpResponse("Данные не валидны")
-    context["order_form"] = OrderForm()
-    return render(request, 'clients/order_djangoform.html', context)
+class CreateOrderView(CreateView):
+    model = Order
+    template_name = 'clients/order_djangoform.html'
+    fields = ["name", "contacts", "description"]
+    success_url = "/orders/"
